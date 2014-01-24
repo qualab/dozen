14cbaaa7214fd2559c4943537eb8a8e19671abb2
qualab <qualab@gmail.com>
2014-01-24 13:50:23 +0400

    Added text::symbol_ref and API for data::set and data::unit.

--- data/text.hpp
+++ data/text.hpp
@@ -71,6 +71,9 @@ namespace data
 
         class symbol_ref;
 
+        symbol_ref get_symbol_at(int index);
+        symbol_ref get_symbol_ref_at(int index);
+
         symbol_ref operator [] (int index);
 
         text& operator += (text another);
@@ -81,32 +84,21 @@ namespace data
         friend text operator * (int count, text source);
 
         text get_substring(int start, int end) const;
+        int find_substring(text substring) const;
+
+        template <class function>
+        void for_each_symbol(function const& functor);
+
+        template <class function>
+        void for_each_symbol(function const& functor) const;
 
         class iterator;
-        class const_iterator;
-        class reverse_iterator;
-        class const_reverse_iterator;
 
         iterator begin();
         iterator end();
 
-        const_iterator begin() const;
-        const_iterator end() const;
-
-        const_iterator cbegin() const;
-        const_iterator cend() const;
-
-        reverse_iterator rbegin();
-        reverse_iterator rend();
-
-        const_reverse_iterator rbegin() const;
-        const_reverse_iterator rend() const;
-
-        const_reverse_iterator crbegin() const;
-        const_reverse_iterator crend() const;
-
-        iterator find(text substring);
-        const_iterator find(text substring) const;
+        iterator begin() const;
+        iterator end() const;
 
     private:
         /// Forward declaration of implementation class in text.cpp file
@@ -115,4 +107,102 @@ namespace data
         /// Lazy initialization and copy-on-write model of the implementation object
         lazy<impl> m_impl;
     };
+
+//  ------------------------------------------------------------------------------
+
+    /// Reference to a symbol in text
+    class DOZEN_API text::symbol_ref
+    {
+    public:
+        /// Initialize symbol reference by reference to a text and index of this symbol
+        symbol_ref(text& owner, int index);
+
+        /// Get code of this symbol in Unicode
+        int get_code() const;
+
+        /// Get this symbol as wide symbol (wchar_t)
+        wchar_t get_as_wchar_t() const;
+
+        /// Get this symbol as ANSI character (char) using specified encoding
+        char get_as_char(std::string const& encoding) const;
+
+        /// Get this symbol as ANSI character (char) using specified encoding
+        char get_as_char(char const* encoding) const;
+
+        /// Get this symbol as ANSI character (char) using defult encoding
+        char get_as_char() const;
+
+        /// Get this symbol as the text object which contains single symbol
+        text get_as_text() const;
+        
+        /// Represent symbol as code in Unicode
+        operator int() const;
+
+        /// Represent symbol as wide char
+        operator wchar_t() const;
+
+        /// Represent symbol as ANSI char using default encoding
+        operator char() const;
+
+        /// Cast symbol to a bool same as integer code of Unicode casts
+        operator bool() const;
+
+        /// Represent symbol as text object
+        operator text() const;
+
+        /// Set symbol as code of Unicode
+        void set_code(int code);
+
+        /// Set symbol as wide character (wchar_t)
+        void set_as_wchar_t(wchar_t code);
+
+        /// Set symbol as ANSI character using specified encoding
+        void set_as_char(char code, std::string const& encoding);
+
+        /// Set symbol as ANSI character using specified encoding
+        void set_as_char(char code, char const* encoding);
+
+        /// Set symbol as ANSI character using default encoding
+        void set_as_char(char code);
+
+        /// Set symbol by single symbol of text object
+        void set_as_text(text const& single);
+
+        /// Initialize symbol by code of Unicode
+        symbol_ref operator = (int code);
+
+        /// Initialize symbol by wide character (wchar_t)
+        symbol_ref operator = (wchar_t code);
+
+        /// Initialize symbol by ANSI charactoer (char)
+        symbol_ref operator = (char code);
+
+        /// Initialize symbol as single symbol of text object
+        symbol_ref operator = (text single);
+
+    private:
+        class impl;
+
+        lazy<impl> m_impl;
+    };
+
+//  ------------------------------------------------------------------------------
+
+    template <class function>
+    void text::for_each_symbol(function const& functor)
+    {
+        for (int i = 0; i < get_length(); ++i)
+        {
+            functor(get_symbol_at(i));
+        }
+    }
+
+    template <class function>
+    void text::for_each_symbol(function const& functor) const
+    {
+        for (int i = 0; i < get_length(); ++i)
+        {
+            functor(get_symbol_at(i));
+        }
+    }
 }
---
+++ data/unit.hpp
@@ -0,0 +1,24 @@
+﻿/// @author Владимир Керимов
+
+#pragma once
+
+#include <data/object.hpp>
+#include <data/lazy.hpp>
+
+namespace data
+{
+    class DOZEN_API unit : public object
+    {
+    public:
+        unit();
+
+        virtual ~unit() override;
+
+        virtual std::unique_ptr<object> clone() const override;
+
+    private:
+        class impl;
+
+        lazy<impl> m_impl;
+    };
+}
